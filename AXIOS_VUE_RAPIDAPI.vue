64086d6e94175e49bbbebacffedd895d4ddb5ab6
yarndev <62940076+yarndev@users.noreply.github.com>
2020-09-23 00:17:40 +0300

    Add files via upload

---
+++ AXIOS_VUE_RAPIDAPI.vue
@@ -0,0 +1,37 @@
+<template>
+
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+    data: function () {
+        return {
+
+            result: null,
+
+        };
+    },
+    methods: {
+        autofill() {
+            axios({
+                    'method': 'GET',
+                    'url': 'https://vindecoder.p.rapidapi.com/decode_vin',
+                    'headers': {
+                        'content-type': 'application/octet-stream',
+                        "x-rapidapi-host": "vindecoder.p.rapidapi.com",
+                        "x-rapidapi-key": "YOUR_API_KEY"
+                    },
+                    'params': {
+                        'YOUR_PARAMETER_KEY': 'YOUR_PARAMETER_KEY',
+                    },
+                })
+                .then((response) => {
+                    console.log(response.data);
+
+                });
+        },
+    },
+};
+</script>
